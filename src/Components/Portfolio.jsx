a5e116580eb83456cb5ccb33d607d2e669f978e0
Vaibhav Ghugretkar <127537492+Vaibhavghugretkar@users.noreply.github.com>
2024-01-25 09:29:21 +0530

    "added images"

--- src/Components/Portfolio.jsx
+++ src/Components/Portfolio.jsx
@@ -19,7 +19,7 @@ import React from "react";
  * Need an image? Check out https://unsplash.com to download a photo you
  * freely use on your site.
  */
-import image from "../images/mlsa pic2.avif";
+import image from "../";
 
 const imageAltText = "desktop with books and laptop";
 
