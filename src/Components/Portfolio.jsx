4e0a41cd7757feaa87ded906baac649dfddab48d
Vaibhav Ghugretkar <127537492+Vaibhavghugretkar@users.noreply.github.com>
2024-01-25 22:47:15 +0530

    "updated changes"

--- src/Components/Portfolio.jsx
+++ src/Components/Portfolio.jsx
@@ -19,13 +19,13 @@ import React from "react";
  * Need an image? Check out https://unsplash.com to download a photo you
  * freely use on your site.
  */
-import image from "../images/motion-background.jpg";
+import image from "../images/mlsa_pic4.jpg";
 
 const imageAltText = "desktop with books and laptop";
 
 /**
  * Project list
- *""
+ *
  * An array of objects that will be used to display for your project
  * links section. Below is a sample, update to reflect links you'd like to highlight.
  */
@@ -61,7 +61,7 @@ const Portfolio = () => {
     <section className="padding" id="portfolio">
       <h2 style={{ textAlign: "center" }}>Portfolio</h2>
       <div style={{ display: "flex", flexDirection: "row", paddingTop: "3rem" }}>
-        <div style={{ maxWidth: "40%", alignSelf: "center" }}>
+        <div style={{ maxWidth: "40%", alignSelf: "center"}}>
           <img
             src={image}
             style={{ height: "90%", width: "100%", objectFit: "cover" }}
