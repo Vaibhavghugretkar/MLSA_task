a77f03cafafcc77d5ef235189456a986ebc68f3f
Vaibhav Ghugretkar <127537492+Vaibhavghugretkar@users.noreply.github.com>
2024-01-25 22:21:57 +0530

    "home pic added"

--- src/Components/Home.jsx
+++ src/Components/Home.jsx
@@ -20,7 +20,7 @@ import PropTypes from "prop-types";
  * Need an image? Check out https://unsplash.com to download a photo you
  * freely use on your site.
  */
-import image from "../images/mlsa_pic.jpg"
+import image from "../images/mlsa_pic1.jpg"
 ;
 
 const imageAltText = "Vaibhav Ghugretkar - I'm a aspiring Front-End Developer and Content Writer. ";
